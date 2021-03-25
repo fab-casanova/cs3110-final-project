73f1a60792d4af831ba5b2b7917bcd34037f7a07
fec24 <fec24@cornell.edu>
2021-03-24 22:27:58 -0400

    Updated board

--- gameboard.ml
+++ gameboard.ml
@@ -1,8 +1,12 @@
 open Player
 open Property
 
-type gameboard = property array
+type gameboard = property list
 
-let get_pos index = 0
+let rec get_index_helper prop board acc =
+  match board with
+  | [] -> raise Not_found
+  | h :: t -> if prop = h then acc else get_index_helper prop t (acc + 1)
 
-let buy_property = 0
+let get_index (prop : property) (board : gameboard) =
+  get_index_helper prop board 0
