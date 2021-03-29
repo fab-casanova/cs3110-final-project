bd13abdb0aeadf52ae24dfbe32b5bce456d937b0
fec24 <fec24@cornell.edu>
2021-03-29 16:30:41 -0400

    Remove roll_dice duplicate

---
+++ _build/game.mli
@@ -0,0 +1,40 @@
+type players
+
+type gameboard
+
+type t
+
+val create_list_of_players : Player.t list -> players
+
+val create_gameboard : Property.t list -> gameboard
+
+val current_player_name : t -> string
+
+val current_player : t -> Player.t
+
+val create_game : gameboard -> players -> t
+
+val num_players : t -> int
+
+val get_start_pos : t -> Property.t
+
+val add_a_player : t -> Player.t -> unit
+
+val get_index : t -> Property.t -> int
+
+val get_prop_at_index : int -> t -> Property.t
+
+val get_players : t -> players
+
+val find_player : string -> players -> Player.t
+
+val move_player : Player.t -> t -> unit
+
+val collect_rent : Player.t -> Player.t -> Property.t -> t -> unit
+
+val collect_tax : Player.t -> Property.t -> t -> unit
+
+(*
+val buying_prompt : 
+*)
+(*TODO: Add auction*)
